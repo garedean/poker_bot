d7a07ff0a2ad87db45e9efa6f03b9b51623d62c8
garedean <garrettdolson@gmail.com>
2015-08-27 10:37:30 -0700

    Add specs and logic for parsing cards into card hash array

--- lib/poker_bot.rb
+++ lib/poker_bot.rb
@@ -2,5 +2,40 @@ require 'pry'
 
 # string array as argument
 def get_best_hand(cards)
+  cards = parse_cards(cards)
+end
 
+# CARD PARSING
+# =============================================================
+
+# turn string array into card hash array
+def parse_cards(cards_string_array)
+  output = []
+
+  cards_string_array.each do |card|
+    output << parse_card(card)
+  end
+
+  output
+end
+
+# returns card as hash with keys of 'value' and 'suit' where values and suits are transformed into integer values
+def parse_card(card_string)
+  card_values = { "2"     => 2,
+                  "3"     => 3,
+                  "4"     => 4,
+                  "5"     => 5,
+                  "6"     => 6,
+                  "7"     => 7,
+                  "8"     => 8,
+                  "9"     => 9,
+                  "10"    => 10,
+                  "jack"  => 11,
+                  "queen" => 12,
+                  "king"  => 13,
+                  "ace"   => 14 }
+
+  card = card_string.split(" of ").map{ |e| e.downcase }
+
+  { value: card_values[card[0]], suit: card[1].to_sym }
 end
--- spec/poker_bot_spec.rb
+++ spec/poker_bot_spec.rb
@@ -1,5 +1,34 @@
 require "poker_bot"
 
-describe("the poker bot") do
+describe "poker bot" do
+  let(:cards_as_string_array) do
+      ["Ace of Diamonds",
+       "King of Diamonds",
+       "Queen of Diamonds",
+       "Jack of Diamonds",
+       "10 of Diamonds"]
+    end
 
+    let(:cards_as_hash_array) do
+      [{value: 14, suit: :diamonds},
+       {value: 13, suit: :diamonds},
+       {value: 12, suit: :diamonds},
+       {value: 11, suit: :diamonds},
+       {value: 10, suit: :diamonds}]
+    end
+
+  # Helper method tests
+  # =========================================
+
+  describe "#parse_card" do
+    it "returns a hash of a card's value (as integer) and suit" do
+      expect(parse_card("Ace of Diamonds")).to eq({value: 14, suit: :diamonds})
+    end
+  end
+
+  describe "#parse_cards" do
+    it "returns an array of hashes representing cards" do
+      expect(parse_cards(cards_as_string_array)).to eq(cards_as_hash_array)
+    end
+  end
 end
